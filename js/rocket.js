9a12833d4161eb3fbf23004d4492260d9466c00f
Amylynne427 <ames427@gmail.com>
2017-08-07 20:52:53 -0400

    Made my rocket!

--- js/rocket.js
+++ js/rocket.js
@@ -5,18 +5,42 @@ var changeState = function (state) {
 	document.body.className = 'body-state'+
 		state;
 		clearInterval(timer);
-
-	if (state == 2) {
-		timer = setInterval(function () {
+		countdownNumber = 10;
 			document.getElementById('countdown').
 			innerHTML = countdownNumber;
-			countdownNumber = countdownNumber - 1;
+
+	// countdown
+	if (state == 2) {
+		timer = setInterval(function () {
+			countdownNumber = countdownNumber 
+			- 1;
+			document.getElementById('countdown').
+			innerHTML = countdownNumber;
+
+
 			if (countdownNumber <= 0) {
 				changeState(3);
 			}
-		}, 1000);
+		}, 500);
 		
 	}else if (state == 3) {
+		var success = setTimeout(function ()
+			{
+				var randomNumber = Math.round
+				(Math.random()*10);
+
+				console.log('randomNumber:',
+					randomNumber)
+
+				//success
+				if (randomNumber > 5) {
+					changeState(4);
+
+				} else {
+					changeState(5); 
+				}
+
+			}, 2000);
 
 	};
 }
